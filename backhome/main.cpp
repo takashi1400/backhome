2588dc2eb4431c9c953c359f2b60919697938135
takashi1400 <the.timewaster.may.be.me@gmail.com>
2015-09-15 23:32:52 +0900

    Initial Commit

---
+++ backhome/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  backhome
+//
+//  Created by takashi on 2015/09/15.
+//  Copyright (c) 2015年 takashi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
